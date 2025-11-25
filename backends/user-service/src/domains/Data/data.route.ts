67e27e86d9481582425111a5bf8c7b842ed40637
Faysal Mridha <fmridha166@gmail.com>
2025-11-25 15:36:26 +0600

    feat: add HomePage component with basic structure

--- backends/user-service/src/domains/Data/data.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import dataController from "./data.controller";
-import { authMiddleware } from "../../middlewares/auth.middleware";
-
-const router = Router();
-
-router.get("/all", authMiddleware, dataController.getAllData);
-
-export default router;
