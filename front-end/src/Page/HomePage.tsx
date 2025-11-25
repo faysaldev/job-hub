9a32b021d4a60eec82852e0e29eb9951290c5c2b
Faysal Mridha <fmridha166@gmail.com>
2025-11-25 21:46:49 +0600

    feat: enhance styling and structure with new global styles, button component, and utility functions

--- front-end/src/Page/HomePage.tsx
+++ front-end/src/Page/HomePage.tsx
@@ -1,5 +1,12 @@
+"use client";
+import { Button } from "@/src/components/ui/button";
 function HomePage() {
-  return <div>HomePage Developing Branch</div>;
+  return (
+    <div>
+      HomePage Developing Branch{" "}
+      <Button className="text-green-400 cursor-pointer">Click Me </Button>
+    </div>
+  );
 }
 
 export default HomePage;
