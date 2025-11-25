59371e959e44b2a485e29cba759cffaafdc6bdf7
Faysal Mridha <fmridha166@gmail.com>
2025-11-25 15:40:38 +0600

    Refactor code structure for improved readability and maintainability

--- backends/message-queue/src/routes/index.ts
+++ backends/message-queue/src/routes/index.ts
@@ -1,12 +1,8 @@
 import { Router } from "express";
 import userRoutes from "../domains/User/user.route";
-import authRoutes from "../domains/Auth/auth.route";
-import dataRoutes from "../domains/Data/data.route";
+
 // Initialize the router
 const router = Router();
 
 router.use("/users", userRoutes);
-router.use("/auth", authRoutes);
-router.use("/data", dataRoutes);
-
 export default router;
