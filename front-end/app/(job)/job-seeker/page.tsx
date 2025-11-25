357e7d35f705a87a2aa298613fe4752b53541db8
Faysal Mridha <fmridha166@gmail.com>
2025-11-25 22:34:55 +0600

    feat: add Job Seeker and Recruiter Dashboard pages with basic structure

---
+++ front-end/app/(job)/job-seeker/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Job Seeker </div>;
+}
+
+export default page;
---
+++ front-end/app/(recruiter)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Recruiter Dashboard</div>;
+}
+
+export default page;
