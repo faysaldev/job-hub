3340b7850b0366f0d30d77573fd617a4f71ea3f5
Faysal Mridha <fmridha166@gmail.com>
2025-11-25 22:32:12 +0600

    feat: create Job Details and Job pages with basic structure

---
+++ front-end/app/(job)/job/[details]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Job Details page</div>;
+}
+
+export default page;
---
+++ front-end/app/(job)/job/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Job page</div>;
+}
+
+export default page;
