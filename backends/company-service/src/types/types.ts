311f0415a5e4f61f7b792d2b647e0c3711a4e724
Faysal Mridha <fmridha166@gmail.com>
2025-12-02 19:05:06 +0600

    [refactor]: removed the company details services over here

--- backends/company-service/src/types/types.ts
+++
@@ -1,9 +0,0 @@
-export interface ProtectedUser {
-  _id: string; // Explicitly typing _id
-  name: string;
-  email: string;
-  password: string;
-  image: string;
-  role: string;
-  phoneNumber: string;
-}
